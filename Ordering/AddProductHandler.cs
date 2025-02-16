30dc704f0104e0edbd89c3c19be2cf91d0778f78
ruben <rubenscheedler@gmail.com>
2025-02-16 13:30:08 +0100

    Add more example code for out of order messages

--- Ordering/AddProductHandler.cs
+++ Ordering/AddProductHandler.cs
@@ -2,14 +2,13 @@
 
 public record AddProduct(Guid OrderId, Guid ProductId);
 
-
 public class AddProductHandler(IOrderRepository repository) : IHandleMessages<AddProduct>
 {
     public async Task Handle(AddProduct command, IMessageHandlerContext context)
     {
         var order = await repository.Get(command.OrderId);
         
-        order.Products.Add(command.ProductId);
+        order.AddProduct(command.ProductId);
 
         await repository.Save(order);
     }
--- Ordering/Order.cs
+++ Ordering/Order.cs
@@ -4,4 +4,22 @@ public class Order
 {
     private Guid Id { get; set; }
     public List<Guid> Products { get; } = [];
+
+    public void AddProduct(Guid productId)
+    {
+        if (!Products.Contains(productId))
+        {
+            Products.Add(productId);
+        }
+    }
+
+    public void RemoveProduct(Guid productId)
+    {
+        if (!Products.Contains(productId))
+        {
+            throw new InvalidOperationException();
+        }
+        
+        Products.Remove(productId);
+    }
 }
